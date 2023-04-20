acd99bf2da323bf614a6f2a6afcbfb8ef6600c24
Yoseph Tapano <yosephman76@gmail.com>
2023-04-20 19:10:12 +0300

    __init__file

---
+++ src/api/v1/views/__init__.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.search import *
+from api.v1.views.hotel_service import *
+from api.v1.views.hotels import *
+from api.v1.views.services import *
+from api.v1.views.users import *
+from api.v1.views.cities import *
+from flask import Blueprint
+from api.v1.views.orders import *
