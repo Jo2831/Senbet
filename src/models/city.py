6ad8669087cb3075a6ad52e81ae9825946e034ef
Yoseph Tapano <yosephman76@gmail.com>
2023-04-20 18:50:03 +0300

    city_model

---
+++ src/models/city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+file: city.py
+Desc: City model
+Authors: Joseph Tapano
+"""
+from models.base import BaseModel
+from models.base import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """Representation for cities"""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    hotels = relationship("Hotel", backref="cities",
+                             cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes City object with super class constructor"""
+        super().__init__(*args, **kwargs)
